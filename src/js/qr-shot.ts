735bd8df6bc452c51c65345de402e1a6e51517b9
Y.Hasegawa <hasegawa@zaf.jp>
2022-02-14 16:28:41 +0900

    shot

--- src/js/qr-shot.ts
+++ src/js/qr-shot.ts
@@ -5,17 +5,42 @@ window.addEventListener('load', async () => {
 
   const preview = document.getElementById('preview')! as HTMLVideoElement
   const previewBox = preview.closest('div.card') as HTMLDivElement
+  const canvas = document.getElementById('shot-canvas') as HTMLCanvasElement
+  const canvasBox = canvas.closest('div.card') as HTMLDivElement
 
-  document.getElementById('facing')!.addEventListener('change', async (evt) => {
-    const facing = evt.target as HTMLSelectElement
+  document.getElementById('start-btn')!.addEventListener('click', async () => {
+    canvasBox.style.display = 'none'
+
+    const facing = document.getElementById('facing') as HTMLSelectElement
     if (!facing.value) {
       return
     }
 
+    previewBox.style.display = 'block'
+
     const stream = await Util.initCamera(facing.value)
+
+    if (preview.srcObject) {
+      preview.pause()
+      preview.currentTime = 0
+      preview.srcObject = null
+    }
     preview.srcObject = stream
     preview.play().catch(e => console.error(e))
+  })
 
-    previewBox.style.display = 'block'
+  document.getElementById('shot-btn')!.addEventListener('click', () => {
+    canvas.width = preview.clientWidth
+    canvas.height = preview.clientHeight
+
+    const context = canvas.getContext('2d')
+    if (!context) {
+      console.error('2d context error')
+      return
+    }
+
+    previewBox.style.display = 'none'
+    context.drawImage(preview, 0, 0, canvas.width, canvas.height)
+    canvasBox.style.display = 'block'
   })
 })
